6ae4deac15e28ffcbde66400688d6d28f872ca83
Ignorant <2734492976@qq.com>
2025-01-09 11:34:43 +0800

    <ploting glue>

--- AI_intro/paper/GLUE.py
+++ AI_intro/paper/GLUE.py
@@ -9,73 +9,72 @@ from tqdm.auto import tqdm
 
 from LogME import LogME
 
-
 models = ["roberta-base", "distilroberta-base", "distilbert-base-uncased", "distilbert-base-cased",
-          "albert-base-v1", "albert-base-v2", "google/electra-base-discriminator", "google/electra-small-discriminator"]
+          "albert-base-v2", "google/electra-base-discriminator", "google/electra-small-discriminator"]
 tasks = ["mnli", "qqp", "qnli", "sst2", "cola", "mrpc", "rte"]
 
 pretrained_models = {
     "roberta-base": {
-        "mnli": "TehranNLP-org/roberta-base-mnli-2e-5-42",
-        "qqp": "TehranNLP-org/roberta-base-qqp-2e-5-42",
-        "qnli": "textattack/roberta-base-QNLI",
-        "sst2": "textattack/roberta-base-SST-2",
-        "cola": "textattack/roberta-base-CoLA",
-        "mrpc": "TehranNLP-org/roberta-base-mrpc-2e-5-42",
-        "rte": "textattack/roberta-base-RTE",
+        "mnli": "TehranNLP-org/roberta-base-mnli-2e-5-42",                      # -0.5340856709158951, 0.8781456953642384
+        "qqp": "TehranNLP-org/roberta-base-qqp-2e-5-42",                        # -0.5614161174849488, 0.9159040316596587
+        "qnli": "textattack/roberta-base-QNLI",                                 # -0.5525178138247662, 0.9267801574226615
+        "sst2": "textattack/roberta-base-SST-2",                                # -0.7155994137768582, 0.9403669724770642
+        "cola": "textattack/roberta-base-CoLA",                                 # -0.40225478819813565, 0.6382594026155579
+        "mrpc": "TehranNLP-org/roberta-base-mrpc-2e-5-42",                      # -0.3543709410468441, 0.8970588235294118
+        "rte": "textattack/roberta-base-RTE",                                   # -0.6421491451004212, 0.7833935018050542
     },
     "distilroberta-base": {
-        "mnli": "rambodazimi/distilroberta-base-finetuned-LoRA-MNLI",
-        "qqp": "Shobhank-iiitdwd/Distilroberta-base-QQP",
-        "qnli": "cross-encoder/qnli-distilroberta-base",
-        "sst2": "azizbarank/distilroberta-base-sst-2-distilled",
-        "cola": "mohammedbriman/distilroberta-base-finetuned-cola",
-        "mrpc": "EugenioRoma/distilroberta-base-mrpc-glue",
-        "rte": "rambodazimi/distilroberta-base-finetuned-LoRA-RTE",
+        "mnli": None,
+        "qqp": "Shobhank-iiitdwd/Distilroberta-base-QQP",                       # -0.738141810989102, 0.6318327974276527
+        "qnli": "cross-encoder/qnli-distilroberta-base",                        # -0.8472257826613385, 0.4946000366099213
+        "sst2": "aal2015/distilroberta-base-sst2-distilled",                    # -0.8194152449059582, 0.9277522935779816
+        "cola": "mohammedbriman/distilroberta-base-finetuned-cola",             # -0.4375926363772261, 0.5788207437251082
+        "mrpc": "EugenioRoma/distilroberta-base-mrpc-glue",                     # -0.6229590671981178, 0.8308823529411765
+        "rte": None,
     },
     "distilbert-base-uncased": {
-        "mnli": "typeform/distilbert-base-uncased-mnli",
-        "qqp": "textattack/distilbert-base-uncased-QQP",
-        "qnli": "textattack/distilbert-base-uncased-QNLI",
-        "sst2": "distilbert/distilbert-base-uncased-finetuned-sst-2-english",
-        "cola": "textattack/distilbert-base-uncased-CoLA",
-        "mrpc": "textattack/distilbert-base-uncased-MRPC",
-        "rte": "Vineetttt/distilbert-base-uncased-finetuned-rte",
+        "mnli": "typeform/distilbert-base-uncased-mnli",                        # -0.2205592725388641, 0.8211920529801324
+        "qqp": "textattack/distilbert-base-uncased-QQP",                        # -0.9129112756344686, 0.5338609943111551
+        "qnli": "textattack/distilbert-base-uncased-QNLI",                      # -0.4704686393263577, 0.8848617975471352
+        "sst2": "distilbert/distilbert-base-uncased-finetuned-sst-2-english",   # -0.2630222378166057, 0.9105504587155964
+        "cola": "textattack/distilbert-base-uncased-CoLA",                      # -0.792119084211421, 0.5685664296893979
+        "mrpc": "textattack/distilbert-base-uncased-MRPC",                      # -0.5298636548772029, 0.8578431372549019
+        "rte": "Vineetttt/distilbert-base-uncased-finetuned-rte",               # -0.833935266212873, 0.5992779783393501
     },
     "distilbert-base-cased": {
         "mnli": None,
-        "qqp": "textattack/distilbert-base-cased-QQP",
-        "qnli": "HeZhang1019/distilbert-base-cased-distilled-qnli-v1",
-        "sst2": "textattack/distilbert-base-cased-SST-2",
-        "cola": "textattack/distilbert-base-cased-CoLA",
-        "mrpc": "textattack/distilbert-base-cased-MRPC",
+        "qqp": "textattack/distilbert-base-cased-QQP",                          # -0.5754286202988488, 0.8974771209497897
+        "qnli": None,   # "HeZhang1019/distilbert-base-cased-distilled-qnli-v1"
+        "sst2": "textattack/distilbert-base-cased-SST-2",                       # -0.8326835243171337, 0.9002293577981652
+        "cola": "textattack/distilbert-base-cased-CoLA",                        # -0.5758762985856327, 0.46372927911071965
+        "mrpc": "textattack/distilbert-base-cased-MRPC",                        # -0.7982118975095636, 0.7843137254901961
         "rte": None,
     },
     "albert-base-v2": {
-        "mnli": "Alireza1044/albert-base-v2-mnli",
-        "qqp": "Alireza1044/albert-base-v2-qqp",
-        "qnli": "Alireza1044/albert-base-v2-qnli",
-        "sst2": "Alireza1044/albert-base-v2-sst2",
-        "cola": "Alireza1044/albert-base-v2-cola",
-        "mrpc": "Alireza1044/albert-base-v2-mrpc",
-        "rte": "Alireza1044/albert-base-v2-rte",
+        "mnli": "Alireza1044/albert-base-v2-mnli",                              # -0.5617629793059487, 0.8448293428425879
+        "qqp": "Alireza1044/albert-base-v2-qqp",                                # -0.6883012127418151, 0.9049715557754143
+        "qnli": "Alireza1044/albert-base-v2-qnli",                              # -0.7711817913454362, 0.9136005857587406
+        "sst2": "Alireza1044/albert-base-v2-sst2",                              # -0.6331057368447117, 0.9231651376146789
+        "cola": "Alireza1044/albert-base-v2-cola",                              # -0.5233847261808, 0.5494768667363472
+        "mrpc": "Alireza1044/albert-base-v2-mrpc",                              # -0.7048722692879975, 0.8627450980392157
+        "rte": "Alireza1044/albert-base-v2-rte",                                # -1.0153694027610904, 0.6750902527075813
     },
     "google/electra-base-discriminator": {
-        "mnli": "TehranNLP-org/electra-base-mnli",
-        "qqp": "TehranNLP-org/electra-base-qqp-2e-5-42",
-        "qnli": "cross-encoder/qnli-electra-base",
-        "sst2": "TehranNLP-org/electra-base-sst2",
-        "cola": "pszemraj/electra-base-discriminator-CoLA",
-        "mrpc": "TehranNLP-org/electra-base-mrpc-2e-5-42",
-        "rte": "anirudh21/electra-base-discriminator-finetuned-rte",
+        "mnli": "TehranNLP-org/electra-base-mnli",                              # -0.1270556164024973, 0.8874172185430463
+        "qqp": "TehranNLP-org/electra-base-qqp-2e-5-42",                        # -0.41978923276613933, 0.9193420727182785
+        "qnli": "cross-encoder/qnli-electra-base",                              # -0.8251125130076153, 0.4946000366099213
+        "sst2": "TehranNLP-org/electra-base-sst2",                              # -0.5110292330649795, 0.9506880733944955
+        "cola": "pszemraj/electra-base-discriminator-CoLA",                     # -0.4206984419701534, 0.6579677841732349
+        "mrpc": "TehranNLP-org/electra-base-mrpc-2e-5-42",                      # -0.549591245533545, 0.8897058823529411
+        "rte": "anirudh21/electra-base-discriminator-finetuned-rte",            # -0.7623099192650991, 0.8231046931407943
     },
     "google/electra-small-discriminator": {
-        "mnli": "howey/electra-small-mnli",
-        "qqp": "howey/electra-small-qqp",
+        "mnli": "howey/electra-small-mnli",                                     # -0.4013931383553602, 0.8119205298013245
+        "qqp": "howey/electra-small-qqp",                                       # -0.6306593387013033, 0.8947069008162256
         "qnli": None,
-        "sst2": "Hazqeel/electra-small-finetuned-sst2",
-        "cola": "pszsemraj/electra-small-discriminator-CoLA",
-        "mrpc": "Intel/electra-small-discriminator-mrpc",
+        "sst2": "Hazqeel/electra-small-finetuned-sst2",                         # -0.6847325826760177, 0.9174311926605505
+        "cola": None,
+        "mrpc": "Intel/electra-small-discriminator-mrpc",                       # -0.6972814945905685, 0.8529411764705882
         "rte": None,
     }
 }
@@ -85,17 +84,14 @@ epoches = 3
 
 
 def main(model_name: str, task_name: str):
-    # if os.path.exists(f"models/glue/fine_tuned_{model_name}_{task_name}.pth"):
-    #     model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    #     model.load_state_dict(torch.load(f"models/glue/fine_tuned_{model_name}_{task_name}.pth"))
-    #     model.to(device)
-    # else:
-    #     model = fine_tune(model_name, task_name)
-
     num_labels = 3 if task_name == "mnli" else 2
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=num_labels)
+    pretrained_model_name = pretrained_models[model_name][task_name]
+    if pretrained_model_name is None:
+        return None, None
+    print(f"Evaluating {model_name} on {task_name} task...")
+    model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name)
     model.to(device)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
     # tokenizer.model_max_length = sys.maxsize
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
     tokenized_datasets = get_dataset(task_name, tokenizer)
@@ -104,7 +100,6 @@ def main(model_name: str, task_name: str):
         shuffle=False, collate_fn=data_collator, batch_size=8, num_workers=4)
     metric = evaluate.load("glue", task_name)
     model.eval()
-    print(f"Evaluating {model_name} on {task_name} task...")
     F, Y = [], []
     for batch in eval_dataloader:
         batch = {k: v.to(device) for k, v in batch.items()}
@@ -191,11 +186,17 @@ def fine_tune(model_name: str, task_name: str):
 
 
 if __name__ == '__main__':
+    count = 0
     for task_name in tasks[4:]:
         result = {}
         print(f"Task: {task_name}")
         for model_name in models:
+            count += 1
+            if count <= 4:
+                continue
             score, accuracy = main(model_name, task_name)
+            if score is None:
+                continue
             result[model_name] = (score, accuracy)
             print(f"{model_name}: {score}, {accuracy}")
         print(result)
--- AI_intro/paper/ploting.py
+++ AI_intro/paper/ploting.py
@@ -3,11 +3,68 @@ import numpy as np
 import re
 
 
+glue_results = {
+    "MNLI": {
+        "RoBERTa": (-0.5340856709158951, 0.8781456953642384),
+        "uncased BERT-D": (-0.2205592725388641, 0.8211920529801324),
+        "ALBERT-v2": (-0.5617629793059487, 0.8448293428425879),
+        "ELECTRA-base": (-0.1270556164024973, 0.8874172185430463),
+        "ELECTRA-small": (-0.4013931383553602, 0.8119205298013245)
+    },
+    "QQP": {
+        "RoBERTa": (-0.5614161174849488, 0.9159040316596587),
+        "RoBERTa-D": (-0.738141810989102, 0.6318327974276527),
+        "uncased BERT-D": (-0.9129112756344686, 0.5338609943111551),
+        "cased BERT-D": (-0.5754286202988488, 0.8974771209497897),
+        "ALBERT-v2": (-0.6883012127418151, 0.9049715557754143),
+        "ELECTRA-base": (-0.41978923276613933, 0.9193420727182785),
+        "ELECTRA-small": (-0.6306593387013033, 0.8947069008162256)
+    },
+    "QNLI": {
+        "RoBERTa": (-0.5525178138247662, 0.9267801574226615),
+        "RoBERTa-D": (-0.8472257826613385, 0.4946000366099213),
+        "uncased BERT-D": (-0.4704686393263577, 0.8848617975471352),
+        "ALBERT-v2": (-0.7711817913454362, 0.9136005857587406),
+        "ELECTRA-base": (-0.8251125130076153, 0.4946000366099213)
+    },
+    "SST-2": {
+        "RoBERTa": (-0.7155994137768582, 0.9403669724770642),
+        "RoBERTa-D": (-0.8194152449059582, 0.9277522935779816),
+        "uncased BERT-D": (-0.2630222378166057, 0.9105504587155964),
+        "cased BERT-D": (-0.8326835243171337, 0.9002293577981652),
+        "ALBERT-v2": (-0.6331057368447117, 0.9231651376146789),
+        "ELECTRA-base": (-0.5110292330649795, 0.9506880733944955),
+        "ELECTRA-small": (-0.6847325826760177, 0.9174311926605505)
+    },
+    "CoLA": {
+        "RoBERTa": (-0.40225478819813565, 0.6382594026155579),
+        "RoBERTa-D": (-0.4375926363772261, 0.5788207437251082),
+        "uncased BERT-D": (-0.792119084211421, 0.5685664296893979),
+        "cased BERT-D": (-0.5758762985856327, 0.46372927911071965),
+        "ALBERT-v2": (-0.5233847261808, 0.5494768667363472),
+        "ELECTRA-base": (-0.4206984419701534, 0.6579677841732349)
+    },
+    "MRPC": {
+        "RoBERTa": (-0.3543709410468441, 0.8970588235294118),
+        "RoBERTa-D": (-0.6229590671981178, 0.8308823529411765),
+        "uncased BERT-D":(-0.5298636548772029, 0.8578431372549019),
+        "cased BERT-D": (-0.7982118975095636, 0.7843137254901961),
+        "ALBERT-v2": (-0.7048722692879975, 0.8627450980392157),
+        "ELECTRA-base": (-0.549591245533545, 0.8897058823529411),
+        "ELECTRA-small": (-0.6972814945905685, 0.8529411764705882)
+    },
+    "RTE": {
+        "RoBERTa": (-0.6421491451004212, 0.7833935018050542),
+        "uncased BERT-D": (-0.833935266212873, 0.5992779783393501),
+        "ALBERT-v2": (-1.0153694027610904, 0.6750902527075813),
+        "ELECTRA-base": (-0.7623099192650991, 0.8231046931407943)
+    }
+}
 glue_tasks = ["MNLI", "QQP", "QNLI", "SST-2", "CoLA", "MRPC", "RTE"]
-glue_models = ["RoBERTa", "RoBERTa-D", "uncased BERT-D", "cased BERT-D", "ALBERT-v1", "ALBERT-v2", "ELECTRA-base", "ELECTRA-small"]
-glue_marker = ['o', 'P', 'd', 'v', 'h', '*', '^', 's']
+glue_models = ["RoBERTa", "RoBERTa-D", "uncased BERT-D", "cased BERT-D", "ALBERT-v2", "ELECTRA-base", "ELECTRA-small"]
+glue_marker = ['o', 'P', 'd', 'v', '*', '^', 's']
 maps = {
-    8: ("results/glue", glue_tasks, glue_models, glue_marker),
+    8: (glue_results, glue_tasks, glue_models, glue_marker),
 }
 
 
@@ -31,33 +88,38 @@ def tau_w(num_of_models: int, T: list, S: list):
     return (2. / (num_of_models * (num_of_models - 1))) * tau
 
 
-def read_data(file_name: str):
-    T = []
-    S = []
-    with open(file_name, "r", encoding="utf-8") as f:
-        for line in f:
-            if line:
-                result = re.findall(r"-?\d+\.\d+", line)
-                if result:
-                    S.append(float(result[0]))
-                    T.append(float(result[1]))
-    return T, S
-
-
 def main(num_of_models: int):
-    T, S = read_data(maps[num_of_models][0])
     tau_ws = []
-    ncols = len(T) // num_of_models
+    ncols = len(maps[num_of_models][1])
+    T = {}
+    S = {}
+    for k, v in maps[num_of_models][0].items():
+        temp_t = []
+        temp_s = []
+        for model_name, (s, t) in v.items():
+            temp_t.append(t)
+            temp_s.append(s)
+        T[k] = temp_t
+        S[k] = temp_s
     for i in range(ncols):
-        tau_ws.append(tau_w(num_of_models, T[i * num_of_models:(i + 1) * num_of_models], S[i * num_of_models:(i + 1) * num_of_models]))
-    plt.subplots(1, ncols, figsize=(20, 2))
-    plt.subplots_adjust(hspace=0.3, wspace=0.5)
+        task_name = maps[num_of_models][1][i]
+        tau_ws.append(tau_w(len(T[task_name]), T[task_name], S[task_name]))
+    fig = plt.figure(figsize=(25, 3))
+    axes = fig.subplots(nrows=1, ncols=ncols)
+    # plt.subplots_adjust(hspace=0.3, wspace=0.5)
+    for i, ax in enumerate(axes):
+        for model_name, (s, t) in maps[num_of_models][0][maps[num_of_models][1][i]].items():
+            ax.scatter(t, s, label=model_name, marker=maps[num_of_models][3][maps[num_of_models][2].index(model_name)], c='black')
+        ax.set_title(f"{maps[num_of_models][1][i]}({tau_ws[i]:.2f})")
+    dots = set()
+    labels = set()
     for i in range(ncols):
-        plt.subplot(1, ncols, i + 1)
-        for j in range(num_of_models):
-            plt.scatter(T[i * num_of_models + j], S[i * num_of_models + j], label=maps[num_of_models][2][j], marker=maps[num_of_models][3][j], c='black')
-        plt.title(f"{maps[num_of_models][1][i]}({tau_ws[i]:.2f})")
-    plt.legend(loc=2, bbox_to_anchor=(1.1, 1.05), borderaxespad=0.)
+        dot, label = fig.axes[i].get_legend_handles_labels()
+        for x, y in zip(dot, label):
+            dots.add(x)
+            labels.add(y)
+    fig.legend(dots, labels, loc='right')
+    # plt.legend(loc=2, bbox_to_anchor=(1.1, 1.05), borderaxespad=0.)
     plt.show()
 
 
